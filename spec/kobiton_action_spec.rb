9bac635f0e6367b7b310905525984e7d2d4c3b69
Jordan Welch <jordanhwelch@gmail.com>
2022-10-05 09:02:45 -0500

    Initial working test

--- spec/kobiton_action_spec.rb
+++ spec/kobiton_action_spec.rb
@@ -1,4 +1,5 @@
 require 'fastlane/action'
+require 'webmock/rspec'
 
 describe Fastlane::Actions::KobitonAction do
   describe '#parse_name' do
@@ -21,6 +22,70 @@ describe Fastlane::Actions::KobitonAction do
       expected = 'x' * 255
 
       expect(parse_name.call(input)).to eq(expected)
+    end
+  end
+
+  describe '#run' do
+    it 'Prints all feedback messages for successful run.' do
+      expect(Fastlane::UI).to receive(:message).with("Getting S3 upload URL...")
+      expect(Fastlane::UI).to receive(:message).with("Got S3 upload URL.")
+      expect(Fastlane::UI).to receive(:message).with("Uploading the build to Amazon S3 storage...")
+      expect(Fastlane::UI).to receive(:message).with("Successfully uploaded the build to Amazon S3 storage.")
+      expect(Fastlane::UI).to receive(:message).with("Successfully uploaded the build to Kobiton!")
+
+
+      base64_authorization = Base64.strict_encode64("username:api_key")
+      authorization = "Basic #{base64_authorization}"
+
+      stub_request(:post, "https://api.kobiton.com/v1/apps/uploadUrl").
+        with(
+          body: {'appId' => '12345', "filename"=>'app.apk'},
+          headers: {
+         'Accept' => 'application/json',
+         'Authorization'=>authorization,
+         'Content-Type' => 'application/x-www-form-urlencoded',
+         'Host' => 'api.kobiton.com',
+         'User-Agent' => 'rest-client/2.1.0 (darwin21.3.0 x86_64) ruby/2.6.10p210'
+          }).
+        to_return(status: 200, body: '{
+          "url": "s3_url",
+          "appPath": "kobiton_app_path"
+        }', headers: {})
+
+      stub_request(:put, "http://s3_url/").
+        with(
+          headers: {
+            'Content-Type' => 'application/octet-stream',
+            'Host' => 's3_url',
+            'User-Agent' => 'rest-client/2.1.0 (darwin21.3.0 x86_64) ruby/2.6.10p210',
+            'X-Amz-Tagging' => 'unsaved=true'
+          }).
+        to_return(status: 200, body: "true", headers: {})
+
+        stub_request(:post, "https://api.kobiton.com/v1/apps").
+        with(
+          body: {"appPath" => "kobiton_app_path", "filename" => "app.apk"},
+          headers: {
+         'Accept' => '*/*',
+         'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'Authorization' => 'Basic dXNlcm5hbWU6YXBpX2tleQ==',
+         'Content-Length' => '41',
+         'Content-Type' => 'application/x-www-form-urlencoded',
+         'Host' => 'api.kobiton.com',
+         'User-Agent' => 'rest-client/2.1.0 (darwin21.3.0 x86_64) ruby/2.6.10p210'
+          }).
+        to_return(status: 200, body: '{
+          "versionId": 12345
+        }', headers: {})
+
+      result = Fastlane::FastFile.new.parse("lane :test do
+        kobiton(
+          api_key: 'api_key',
+          app_id: 12345,
+          file: '../spec/resources/app.apk',
+          username: 'username',
+        )
+      end").runner.execute(:test)
     end
   end
 end
